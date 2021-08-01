8cd84cdf4a49ce051bba0c3a66948ab23b497366
soyoung12 <50661149+soyoung12@users.noreply.github.com>
2021-08-01 21:42:09 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+#폴더 안에 있는 __init__.py는 소스코드가 여러개 일 경우 하나로 묶는 역할
+
+
+
+#import inspect
+#import os
+#import sys
+
+    from ConfigHelper import ConfigHelper as Config
+except ImportError as e:
+    print(e," 추가할 수 없습니다.")
+    exit(1)
