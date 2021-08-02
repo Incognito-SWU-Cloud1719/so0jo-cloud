95fc05dea6363532295b6c88ebbdeb87bdc7507a
soyoung12 <50661149+soyoung12@users.noreply.github.com>
2021-08-02 23:03:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ os.system('cd -f makefile2  ~/vsftpd-2.3.4-infected/Makefile')
 
 #os.system('useradd nobody')
 os.system('sudo mkdir /usr/share/empty')
+os.system(' cd vsftpd-2.3.4-infected/')
 os.system('sudo cp vsftpd /usr/local/sbin/vsftpd')
 os.system('sudo cp vsftpd.8 /usr/local/man/man8')
 
