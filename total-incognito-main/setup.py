d711c358cb2fc73e3e80c5756f41f8b9e4747f3f
soyoung12 <50661149+soyoung12@users.noreply.github.com>
2021-08-03 12:16:33 +0900

    Add files via upload

---
+++ total-incognito-main/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='jupyternootebook',
+      version='0.0.1',
+      url='https://github.com/Incognito-SWU-Cloud1719',
+      author='Cloud1719',
+      description='Can use it to install sftp port service from Python.',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      install_requires=['cython'],
+      zip_safe=False,
+      classifiers=[
+          'License :: OSI Approved :: GNU License'
+      ]
+)
